1b965c23ea322c4774320a3a792eb405a9aa2908
Julien Semaan <jul.semaan@gmail.com>
2013-11-28 08:24:20 -0500

    Use the node's pid value for $user_name

--- lib/pf/vlan.pm
+++ lib/pf/vlan.pm
@@ -343,6 +343,7 @@ sub getNormalVlan {
     elsif (defined $node_info->{'pid'}){
         $logger->info("Looking up roles based on the node pid");
         if(!defined $ssid){$ssid=""}
+        $user_name = $node_info->{'pid'};
         my $params = {
             username => $node_info->{'pid'},
             connection_type => connection_type_to_str($connection_type),
